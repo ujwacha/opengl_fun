c170e10e41f5b7cb54f75dce13d1584f03c5a652
ujwacha <ujwolacharya4@gmail.com>
2025-03-07 12:17:00 +0545

    Dracula theme and better lines

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <GL/freeglut_std.h>
 #include <GL/gl.h>
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
@@ -14,6 +15,7 @@
 #include <vector>
 
 #define PI 3.14159
+#define CIRCLE_RAD 0.1
 
 // Dracula Theme Colors
 RGB dracula_bg(0.15686f, 0.1647f, 0.21176f);    // #282A36
@@ -32,16 +34,21 @@ void RenderString(float x, float y, void *font, const char* string, RGB rgb) {
 }
 
 void draw_line(GLfloat cx, GLfloat cy, GLfloat cxx, GLfloat cyy, RGB rgb) {
+  glLineWidth(5.0f); 
+  
   glBegin(GL_LINE_LOOP);
   glColor3f(rgb.r, rgb.g, rgb.b); 
   glVertex3f(cx, cy, 0);
   glVertex3f(cxx, cyy, 0);
   glEnd();
+
+  glLineWidth(1.0f); 
 }
+
 
 void draw_poly(GLfloat cx, GLfloat cy, GLfloat r, int num, RGB rgb) {
   float offset_angle = 2 * PI / num;
-  glBegin(GL_LINE_LOOP);
+  glBegin(GL_POLYGON);
   glColor3f(rgb.r, rgb.g, rgb.b); 
   for(int i = 0; i < num; i++) {
     float current_angle = i*offset_angle;
@@ -52,7 +59,7 @@ void draw_poly(GLfloat cx, GLfloat cy, GLfloat r, int num, RGB rgb) {
   glEnd();
 }
 
-void draw_circle(GLfloat cx, GLfloat cy, GLfloat r, RGB rgb) { 
+void draw_circle(GLfloat cx, GLfloat cy, GLfloat r, RGB rgb) {
   draw_poly(cx, cy, r, 50, rgb);
 }
 
@@ -64,7 +71,20 @@ bool is_in(int key, std::vector<Graph_Node*> &nodes) {
 }
 
 void draw_edge(Edge* e) {
-  draw_line(e->end1->cx, e->end1->cy, e->end2->cx, e->end2->cy, e->color);
+  // make vector and unit vector
+  float vx = e->end2->cx - e->end1->cx;
+  float vy = e->end2->cy - e->end1->cy;
+
+  float magnetude = std::sqrt(vx*vx + vy*vy);
+  vx/= magnetude;
+  vy/= magnetude;
+
+
+  draw_line(e->end1->cx + vx*CIRCLE_RAD,
+	    e->end1->cy + vy*CIRCLE_RAD,
+	    e->end2->cx - vx*CIRCLE_RAD,
+	    e->end2->cy - vy*CIRCLE_RAD,
+	    e->color);
 }
 
 void draw_graph(Graph_Node *g, bool first = true, bool already_checked = false) {
@@ -72,11 +92,11 @@ void draw_graph(Graph_Node *g, bool first = true, bool already_checked = false) 
   if (first) nodes.clear();
   if (!already_checked && is_in(g->key, nodes)) return;
   
-  draw_circle(g->cx, g->cy, 0.1, g->color);
+  draw_circle(g->cx, g->cy, CIRCLE_RAD, g->color);
   
   char format[20];
   sprintf(format, "%i", g->value);
-  RenderString(g->cx, g->cy, GLUT_BITMAP_TIMES_ROMAN_10, format, dracula_fg); 
+  RenderString(g->cx, g->cy, GLUT_BITMAP_TIMES_ROMAN_24, format, dracula_fg); 
 
   nodes.push_back(g);
 
@@ -97,7 +117,7 @@ void renderScene(void) {
 
     // Create nodes with Dracula colors
     Graph_Node g = Graph_Node_new(10, -0.5f, 0.0f, dracula_cyan);
-    Graph_Node g2 = Graph_Node_new(20, 0.5f, 0.0f, dracula_yellow);
+    Graph_Node g2 = Graph_Node_new(20, 0.5f, 0.0f, dracula_cyan);
 
     // Create edge with Dracula pink
     Edge e = edge_new(&g, &g2, dracula_pink);
